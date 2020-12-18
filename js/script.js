5e42966f54234c51789da45a1f43f515bb79444c
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-18 12:47:08 +0100

    start repo

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Esercizio:
+// Traccia e consigli nella cartella "Slide Pdf" su Drive, file: Boolfix-milestone1.rtf
+// Poster:
+// L'API ci restituirà url relativo dell'immagine, come prefisso aggiungiamo:
+// https://image.tmdb.org/t/p/w220_and_h330_face/ (formato: 220px X 330px)
