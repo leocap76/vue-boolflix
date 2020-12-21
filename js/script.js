9e82d84ae3c845517dd1337dc04080701ae6edb9
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-21 15:42:23 +0100

    js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ var root = new Vue(
       imgPrefix: 'https://image.tmdb.org/t/p/w220_and_h330_face',
     },
     methods: {
+
       searchMovie: function() {
         var self = this;
         var input = self.search;
@@ -23,9 +24,10 @@ var root = new Vue(
         {
           params: {
             query: self.search,
+            api_key: '8f719a955600e8912d95a9391e81e28a',
+            language: 'it-IT',
           }
-        }
-      )
+        })
         .then(function (result) {
           console.log(result.data.results);
           self.search = '';
@@ -36,3 +38,38 @@ var root = new Vue(
   },
 
 });
+
+ // ____________ESEMPIO____________________
+// var app = new Vue({
+//   el:'#root',
+//   data: {
+//     discs: [],
+//     genres: [],
+//       valueGenre: '',
+//   },
+//   methods:{
+//
+//   },
+//   mounted: function(){
+//     var self = this;
+//
+//     axios
+//     .get('https://flynn.boolean.careers/exercises/api/array/music')
+//     .then(function(result){
+//       self.discs = result.data.response;
+//       console.log(self.discs);
+//
+//       selff.discs.sort( function (disc1, disc2){
+//         return parseInt(disc1.year) - parseInt(disc2.year);
+//       });
+//
+//      });
+//
+//     self.discs.forEach(
+//     (element) => {
+//       if (!self.genres.includes(element.genre))
+//       self.genres.push(element.genre);
+//       }
+//     );
+//   }
+// });
